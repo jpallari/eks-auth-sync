6368914f947a70fc423264e7f7093159170d02d3
Jaakko Pallari <jkpl@lepovirta.org>
2020-03-21 23:30:33 +0200

    Scripts for bootstrapping the dev env and tests

---
+++ bin/bootstrap.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -euo pipefail
+PROJECT_ROOT="$(dirname "$0")/.."
+
+# Prepare virtualenv
+if [ -d "$PROJECT_ROOT/.env" ]; then
+    python3 -m venv "$PROJECT_ROOT/.env"
+fi
+. "$PROJECT_ROOT/.env/bin/activate"
+
+# Install dependencies
+set -x
+python3 -m pip install -r requirements.txt
---
+++ bin/test.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -euo pipefail
+PROJECT_ROOT="$(dirname "$0")/.."
+. "$PROJECT_ROOT/.env/bin/activate"
+
+# Run tests etc.
+set -x
+black --check src/ tests/ setup.py
+pylint --rcfile="$PROJECT_ROOT/pylintrc" src/ tests/ setup.py
+mypy src/
+python3 setup.py test
