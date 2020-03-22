41d6e9243d6773eb9a2fdf587e86d9061ff7539f
Jaakko Pallari <jkpl@lepovirta.org>
2020-03-22 15:22:47 +0200

    Run test tools from installed modules

--- bin/test.sh
+++ bin/test.sh
@@ -4,8 +4,8 @@ PROJECT_ROOT="$(dirname "$0")/.."
 
 # Run tests etc.
 set -x
-black --check src/ tests/ setup.py
-mypy src/
-pylint --rcfile="$PROJECT_ROOT/pylintrc" src/ tests/ setup.py
+python3 -m black --check src/ tests/ setup.py
+python3 -m mypy src/
+python3 -m pylint --rcfile="$PROJECT_ROOT/pylintrc" src/ tests/ setup.py
 python3 setup.py test --test-suite tests.unit
 python3 setup.py test --test-suite tests.integration
