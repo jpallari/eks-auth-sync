fcd3ac35d7404d90da00d11bd8ed041b0bcc08f6
Jaakko Pallari <jkpl@lepovirta.org>
2020-03-22 14:41:16 +0200

    Basic CircleCI setup

--- bin/test.sh
+++ bin/test.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 set -euo pipefail
 PROJECT_ROOT="$(dirname "$0")/.."
-. "$PROJECT_ROOT/.env/bin/activate"
 
 # Run tests etc.
 set -x
 black --check src/ tests/ setup.py
-pylint --rcfile="$PROJECT_ROOT/pylintrc" src/ tests/ setup.py
 mypy src/
+pylint --rcfile="$PROJECT_ROOT/pylintrc" src/ tests/ setup.py
 python3 setup.py test --test-suite tests.unit
 python3 setup.py test --test-suite tests.integration
