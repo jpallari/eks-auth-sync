84ef8f0030881497cfbc5c0698a579508d5edfc0
Jaakko Pallari <jkpl@lepovirta.org>
2020-03-22 22:00:27 +0200

    Reorder CLI args

--- src/eks_auth_sync/_args.py
+++ src/eks_auth_sync/_args.py
@@ -14,24 +14,6 @@ def parser() -> argparse.ArgumentParser:
         "--cluster", dest="cluster", required=True, help="Cluster to update",
     )
     aparser.add_argument(
-        "--update",
-        dest="update",
-        action="store_true",
-        help="Update cluster instead of printing the AWS auth details",
-    )
-    aparser.add_argument(
-        "--log-format",
-        dest="log_format",
-        default="text",
-        help='Logging format. Either "json" or "text". Default: text',
-    )
-    aparser.add_argument(
-        "--log-level",
-        dest="log_level",
-        default="WARNING",
-        help="Logging level. Default: WARNING",
-    )
-    aparser.add_argument(
         "--scan-roles-path",
         dest="roles_path",
         help="AWS IAM role path to scan for EKS users",
@@ -40,6 +22,12 @@ def parser() -> argparse.ArgumentParser:
         "--scan-users-path",
         dest="users_path",
         help="AWS IAM user path to scan for EKS users",
+    )
+    aparser.add_argument(
+        "--update",
+        dest="update",
+        action="store_true",
+        help="Update cluster instead of printing the AWS auth details",
     )
     aparser.add_argument(
         "--allow-empty",
@@ -63,6 +51,18 @@ def parser() -> argparse.ArgumentParser:
         "--auth-role-arn",
         dest="auth_role_arn",
         help="Role to assume for EKS authentication",
+    )
+    aparser.add_argument(
+        "--log-format",
+        dest="log_format",
+        default="text",
+        help='Logging format. Either "json" or "text". Default: text',
+    )
+    aparser.add_argument(
+        "--log-level",
+        dest="log_level",
+        default="WARNING",
+        help="Logging level. Default: WARNING",
     )
     aparser.add_argument("--region-name", dest="region_name", help="AWS region to use")
     return aparser
